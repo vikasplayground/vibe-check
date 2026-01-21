b4920172e5e9de833686d33564c7d3ed18c5d636
vikasplayground <vikasplayground@gmail.com>
2026-01-21 16:19:01 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'vibe-check-v1';
+const ASSETS_TO_CACHE = [
+    './',
+    './index.html',
+    './manifest.json'
+];
+
+// Install Event: Cache assets
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log('Opened cache');
+            return cache.addAll(ASSETS_TO_CACHE);
+        })
+    );
+});
+
+// Fetch Event: Serve from cache if offline
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            // Return cached version or fetch from network
+            return response || fetch(event.request);
+        })
+    );
+});
+
+// Activate Event: Cleanup old caches
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
